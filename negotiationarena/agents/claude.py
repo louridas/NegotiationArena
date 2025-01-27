e6745edfc218ddb8b5dcd7fc4254e02babc3dc3c
Panos Louridas <louridas@gmail.com>
2025-01-27 11:59:03 +0200

    add support for AWS anthropic

--- negotiationarena/agents/claude.py
+++ negotiationarena/agents/claude.py
@@ -1,5 +1,5 @@
 import os
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from anthropic import Anthropic, AnthropicBedrock, HUMAN_PROMPT, AI_PROMPT
 from negotiationarena.agents.agents import Agent
 import time
 from copy import copy, deepcopy
@@ -12,6 +12,7 @@ class ClaudeAgent(Agent):
         agent_name: str,
         model: str = "claude-2.1",
         use_system_prompt=True,
+        api_type: str = "anthropic"
     ):
         super().__init__(agent_name)
         self.run_epoch_time_ms = str(round(time.time() * 1000))
@@ -19,15 +20,33 @@ class ClaudeAgent(Agent):
         self.conversation = []
         self.model = model
         self.use_system_prompt = use_system_prompt
-        self.role_to_prompt = {
-            "user": HUMAN_PROMPT,
-            "assistant": AI_PROMPT,
-        }
+        self.api_type = api_type
+        if self.api_type == "anthropic":
+            self.role_to_prompt = {
+                "user": HUMAN_PROMPT,
+                "assistant": AI_PROMPT,
+            }
+        elif self.api_type == "amazon_bedrock":
+            self.role_to_prompt = {
+                "user": "user",
+                "assistant": "assistant",
+            }
+        else:
+            raise ValueError(
+                "api_type must be either 'anthropic' or 'amazon_bedrock'"
+            )
         self.prompt_entity_initializer = "system"
-        self.anthropic = Anthropic(
-            # defaults to os.environ.get("ANTHROPIC_API_KEY")
-            api_key=os.environ.get("ANTHROPIC_API_KEY"),
-        )
+        if self.api_type == "anthropic":
+            self.anthropic = Anthropic(
+                # defaults to os.environ.get("ANTHROPIC_API_KEY")
+                api_key=os.environ.get("ANTHROPIC_API_KEY"),      
+            )
+        elif self.api_type == "amazon_bedrock":
+            self.anthropic = AnthropicBedrock(
+                aws_access_key=os.environ.get("AWS_ACCESS_KEY_ID"),
+                aws_secret_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+                aws_region="us-west-2"
+            )     
 
     def init_agent(self, system_prompt, role):
         if AGENT_ONE in self.agent_name:
@@ -58,8 +77,10 @@ class ClaudeAgent(Agent):
         result = cls.__new__(cls)
         memo[id(self)] = result
         for k, v in self.__dict__.items():
-            if (type(v)) == Anthropic:
+            if (type(v) == Anthropic): 
                 v = "AnthropicObject"
+            if (type(v) == AnthropicBedrock):
+                v = "AnthropicBedrockObject"
             setattr(result, k, deepcopy(v, memo))
         return result
 
@@ -94,17 +115,57 @@ class ClaudeAgent(Agent):
 
         return prompt + f"\n\n{self.role_to_prompt['assistant']}"
 
-    def chat(self):
-        t = self.messages_to_prompt(self.conversation)
+    def messages_to_anthropic_format(self, messages):
 
-        completion = self.anthropic.completions.create(
-            model=self.model,
-            max_tokens_to_sample=400,
-            temperature=0.7,
-            prompt=t,
-        )
+        anthropic_messages = []
+        if self.use_system_prompt:
+            system_prompt = self.conversation[0]["content"]
+
+            for message in messages[1:]:
+                anthropic_messages.append({
+                    "role": self.role_to_prompt[message["role"]],
+                    "content": message["content"]
+                })
+        else:
+            anthropic_messages.append({
+                "role": "assistant",
+                "content": self.conversation[0]["content"]
+            })
+
+            for message in messages[2:]:
+                anthropic_messages.append({
+                    "role": self.role_to_prompt[message["role"]],
+                    "content": message["content"]
+                })
+
+        return system_prompt, anthropic_messages
+
+    def chat(self):
+
+        if self.api_type == "anthropic":
+            t = self.messages_to_prompt(self.conversation)
+            completion = self.anthropic.completions.create(
+                model=self.model,
+                max_tokens_to_sample=400,
+                temperature=0.7,
+                prompt=t,
+            )
+            completion = completion.completion
+        else:
+            system_prompt, messages = self.messages_to_anthropic_format(
+                self.conversation)
+            response = self.anthropic.messages.create(
+                model=self.model,
+                system=system_prompt,
+                max_tokens=400,
+                temperature=0.7,
+                messages=messages
+            )
+            completion = ""
+            for content_block in response.content:
+                completion += content_block.text
         time.sleep(0.2)
-        return completion.completion
+        return completion
 
     def update_conversation_tracking(self, role, message):
         self.conversation.append({"role": role, "content": message})
